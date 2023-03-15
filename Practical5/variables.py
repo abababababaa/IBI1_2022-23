3c3b68ba5790adb638e823f8f99880459e9dff88
abababababaa <127284132+abababababaa@users.noreply.github.com>
2023-03-15 20:57:28 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,3 +3,12 @@ b = -118.24
 c = 116.39
 d = a - b
 e = c - a
+    # e > d, so Rob traveled to Haining is further than to Los Angeles
+  
+X = True
+Y = False
+W = X and Y
+Z = X or Y
+
+
+#    W is False, Z is True
